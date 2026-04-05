517efd4f3d745fc071eea916cdd2e9b938e23a7b
Xtwo-oss <siminroman68@gmail.com>
2026-04-05 20:17:01 +0500

    Create pythonscript.py

---
+++ pythonscript.py
@@ -0,0 +1,9 @@
+print("hello from GitHub!")
+while a != "hello"
+    a = input()
+    if a == "hello":
+        print("it's from git")
+    else:
+        print(a)
+        print("enter hello")
+print("good bye!")
