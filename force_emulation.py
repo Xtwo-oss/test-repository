b0376f5a467f85b5187c4752cbf724cc697200f0
Xtwo-oss <siminroman68@gmail.com>
2026-04-19 10:53:28 +0500

    Update force_emulation.py

--- force_emulation.py
+++ force_emulation.py
@@ -8,14 +8,14 @@ while True:
     if p == 0:
         p = 1.225
     cd = 1.0
-    h = float(input("enter your height:")) #like 1.23 (123 cm)
-    w = float(input("enter your width:")) #like 1.23 (123 cm)
+    h = float(input("enter height:")) #like 1.23 (123 cm)
+    w = float(input("enter width:")) #like 1.23 (123 cm)
     a = int(input("is this a body?(0/1):"))
     if a == 1:
         S = h*w*0.8
     elif a == 0:
         S = h*w
-    m = float(input("enter your mass:")) #like 123 (123 kg)
+    m = float(input("enter mass:")) #like 123 (123 kg)
     t = 0.01
     k = (p*cd*S)/(2*m)
     for tm in range(int(input("enter time:"))*100):
@@ -23,6 +23,6 @@ while True:
         if a < 0:
             a = 0
         v += a * t
-        print(f"velocity: {v:0<17}kmh, time: {(tm+1)/100:0<5}")
+        print(f"velocity: {v:0<18}mps, time: {(tm+1)/100:0<5}")
         slp(0.01)
     print("[program end]")
