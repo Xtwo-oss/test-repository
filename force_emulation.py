6f02b797bf6243d456457e36e91da3084fb45f34
Xtwo-oss <siminroman68@gmail.com>
2026-04-19 11:21:47 +0500

    Update force_emulation.py

--- force_emulation.py
+++ force_emulation.py
@@ -8,9 +8,9 @@ while True:
     if p == 0:
         p = 1.225
     cd = 1.0
-    h = float(input("Enter the height (or length) of the object (the higher the slower the object):")) #like 1.23 (123 cm)
+    h = float(input("Enter the height (or length) of the object (the higher the height, the slower the object):")) #like 1.23 (123 cm)
     w = float(input("Enter the width of the object (the higher the width, the slower the object):")) #like 1.23 (123 cm)
-    a = float(input("Enter the object's streamlining (the higher the object's roundness, the rounder it is):"))
+    a = float(input("Enter the object's streamlining (the lower the object's roundness, the rounder it is):"))
     S = h*w*a
     m = float(input("Enter the weight of the object (the higher the weight, the faster the object):")) #like 123 (123 kg)
     t = 0.01
