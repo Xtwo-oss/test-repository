ca34f4b646fe7f50cd9f0d4c2a5c491b8cad48ad
Xtwo-oss <siminroman68@gmail.com>
2026-04-19 10:48:52 +0500

    Update force_emulation.py

--- force_emulation.py
+++ force_emulation.py
@@ -1,13 +1,21 @@
 from time import sleep as slp
 while True:
     v = 0
-    g = float(input("enter g:"))
-    p = float(input("enter p:"))
+    g = float(input("enter g:")) #0 for standart
+    if g == 0:
+        g = 9.8
+    p = float(input("enter p:")) #0 for standart
+    if p == 0:
+        p = 1.225
     cd = 1.0
-    h = float(input("enter your height:"))
-    w = float(input("enter your width:"))
-    S = h*w*0.8
-    m = float(input("enter your mass:"))
+    h = float(input("enter your height:")) #like 1.23 (123 cm)
+    w = float(input("enter your width:")) #like 1.23 (123 cm)
+    a = int(input("is this a body?(0/1):"))
+    if a == 1:
+        S = h*w*0.8
+    elif a == 0:
+        S = h*w
+    m = float(input("enter your mass:")) #like 123 (123 kg)
     t = 0.01
     k = (p*cd*S)/(2*m)
     for tm in range(int(input("enter time:"))*100):
@@ -15,6 +23,6 @@ while True:
         if a < 0:
             a = 0
         v += a * t
-        print(f"velocity: {v:0<20}, time: {(tm+1)/100:0<5}")
+        print(f"velocity: {v:0<17}kmh, time: {(tm+1)/100:0<5}")
         slp(0.01)
     print("[program end]")
